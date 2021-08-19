d85978d7054100c1c18b787aa6adc68c06913ef0
mj <eunoia2040@gmail.com>
2021-08-19 21:21:27 +0900

    Edit: new lines

--- coordinates/main.js
+++ coordinates/main.js
@@ -1,9 +1,6 @@
 const $vertical = document.querySelector('.vertical');
-
 const $horizontal = document.querySelector('.horizontal');
-
 const $target = document.querySelector('.target');
-
 const $tag = document.querySelector('.tag');
 
 document.addEventListener('mouseover', (e) => {
