ea4fa5e7dfa4b936ec8e25d343765ffb5199ed63
mj <eunoia2040@gmail.com>
2021-08-19 21:20:46 +0900

    add: coordinates mini project

---
+++ coordinates/main.js
@@ -0,0 +1,22 @@
+const $vertical = document.querySelector('.vertical');
+
+const $horizontal = document.querySelector('.horizontal');
+
+const $target = document.querySelector('.target');
+
+const $tag = document.querySelector('.tag');
+
+document.addEventListener('mouseover', (e) => {
+    const X = e.clientX;
+    const Y = e.clientY;
+
+    $vertical.style.left = `${X}px`;
+    $horizontal.style.top = `${Y}px`;
+
+    $target.style.left = `${X}px`;
+    $target.style.top = `${Y}px`;
+    
+    $tag.style.left = `${X}px`;
+    $tag.style.top = `${Y}px`;
+    $tag.textContent = `${X}px ${Y}px`;
+});
